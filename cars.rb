5d89614a238ce5a0913d5f9d64f83ccbd1d17947
John Caldwell <caldwelljs@gmail.com>
2013-11-18 18:11:22 -0600

    create hash for cars per color with broken color tracker

--- cars.rb
+++ cars.rb
@@ -1,19 +1,36 @@
 class Car
 @@total_car_count = 0
-	
+@@cars_per_color = {}
+attr_accessor :color
+
 	def to_s()
 		"I'm a car! I've driven #{@distance} and have #{@fuel} gallons gas left"
 	end
 
-	def initialize()
+	def initialize(color)
+		@color = color
 		@fuel = 10
 		@distance = 0
-		@@total_car_count += 1
+		@@total_car_count += 1	
+		
+		if @@cars_per_color[color] == nil
+			@@cars_per_color[color] = 1
+		else
+			@@cars_per_color[color] += 1
+		end
+		@@convertible = true
 	end
+
+
+	def self.most_popular_color
+		@@cars_per_color.sort_by {|key, value| value }
+		@@cars_per_color.keys[0]
+	end
+
 
 	def self.total_car_count
        @@total_car_count
-    end
+  	end
 
 	def drive(miles)
 		if (@fuel - miles/20.0) >= 0
@@ -31,12 +48,31 @@ class Car
 		puts "You must pay $#{3.5 * gallons_needed}"
 		@fuel = 10.0
 	end
+
 end
 
-# car_a = Car.new()
-# car_b = Car.new()
-# puts car_a
-# puts car_b
+class Convertible
+	@roof_status = "down"
+
+def top_down
+
+car_a = Car.new("black")
+car_b = Car.new("blue")
+car_c = Car.new("blue")
+car_d = Car.new("blue")
+car_e = Car.new("red")
+
+
+best_color = Car.most_popular_color
+Car.new(best_color)
+puts best_color
+
+
+
+
+# Car.cars_per_color["black"]
+
+
 # car_a.drive(10)
 # puts car_a
 # puts car_b
@@ -47,8 +83,12 @@ end
 
 
 
-puts Car.total_car_count
-c1 = Car.new
-puts Car.total_car_count
-c2 = Car.new
-puts Car.total_car_count
+# puts Car.total_car_count
+# c1 = Car.new
+# puts Car.total_car_count
+# c2 = Car.new
+# puts Car.total_car_count
+
+
+
+
